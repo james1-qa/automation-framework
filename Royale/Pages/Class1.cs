822854e0c6b1761dcb92fe3d060d46d1ba5af430
james_qa_ <james.houghton@stickyeyes.com>
2020-11-21 14:20:31 +0000

    Added new project for Page Object Model

---
+++ Royale/Pages/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Royale.Pages
+{
+    public class HeaderNav
+    {
+
+    }
+
+    public class HeaderNavMap
+    {
+
+    }
+}
